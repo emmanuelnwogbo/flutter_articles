2fca367ed3cdd947d233fe7ef27ac6f728e8fd48
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2020-12-10 00:36:00 +0100

    base form

---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  createState() {
+    return LoginScreenState();
+  }
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  final formKey = GlobalKey<FormState>();
+
+  String email = "";
+  String password = "";
+
+  Widget build(context) {
+    return Container(
+        margin: EdgeInsets.all(20.0),
+        child: Form(
+            key: formKey,
+            child: Column(
+              children: <Widget>[
+                emailField(),
+                passwordField(),
+                Container(
+                  margin: EdgeInsets.only(top: 25.0),
+                ),
+                submitButton()
+              ],
+            )));
+  }
+
+  Widget emailField() {
+    return TextFormField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+            labelText: 'Email Address', hintText: 'you@example.com'),
+        validator: (String value) {
+          if (!value.contains('@')) {
+            return 'Please enter a valid email';
+          }
+        },
+        onSaved: (String value) {
+          email = value;
+        });
+  }
+
+  Widget passwordField() {
+    return TextFormField(
+      obscureText: true,
+      decoration: InputDecoration(labelText: 'Password', hintText: 'Password'),
+      validator: (String value) {
+        if (value.length < 4) {
+          return 'Please enter a valid email';
+        }
+      },
+      onSaved: (String value) {
+        password = value;
+        print(email);
+        print(password);
+        print('these are the values here');
+      },
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      color: Colors.blue,
+      child: Text('Submit'),
+      onPressed: () {
+        if (formKey.currentState.validate()) {
+          formKey.currentState.save();
+        }
+      },
+    );
+  }
+}
